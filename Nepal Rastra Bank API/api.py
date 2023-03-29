20d5b1a61178a5276da07bdfb06743dba2b48c7f
Rupesh Bhandari <rpshbhandari@gmail.com>
2023-03-29 18:27:22 +0545

    updated for user input

--- Nepal Rastra Bank API/api.py
+++ Nepal Rastra Bank API/api.py
@@ -13,46 +13,12 @@ class Currency():
         self.data = self.data.json()
 
     def get_data(self):
-        for i in self.data['data']:
-
-            for j in self.data['data'][i]['rates']:
-                
-                if j['currency']['iso3'] == self.currency:
-                    print(f"Currency Code: {j['currency']['iso3']}")
-                    print(f"Currency Name: {j['currency']['name']}")
-                    print(f"Currency Unit: {j['currency']['unit']}")
-                    print(f"Currency Buy : {j['buy']}")
-                    print(f"Currency Sell: {j['sell']}")
-                else:
-                    for a in self.data['data'][i]['rates']:
-                        print(f"Currency Code: {a['currency']['iso3']}")
-                        print(f"Currency Name: {a['currency']['name']}")
-                        print(f"Currency Unit: {a['currency']['unit']}")
-                        print(f"Currency Buy : {a['buy']}")
-                        print(f"Currency Sell: {a['sell']}")
-                    
-        return self.data
-    
-    def convert_to_npr(self, amount):
-        self.amount = amount
-        for i in self.data['data']:
-            
-            for j in self.data['data'][i]['rates']:
-                
-                # Filter the currency from argument and convert it to NPR
-                if j['currency']['iso3'] == self.currency:
-                    print(f"'Currency Name:' {j['currency']['name']}")
-                    print(f"'Currency Buy:' {float(j['buy']) * amount/float(j['currency']['unit'])}")
-                    print(f"'Currency Sell:' {float(j['sell']) * amount/float(j['currency']['unit'])}")
-
-    
-    def table(self):
         currency_code = list()
         currency_name = list()
         currency_buy = list()
         currency_sell = list()
         
-        if self.currency != '':
+        if self.currency != '' and self.currency != 'ALL':
             for i in self.data['data']['payload']['rates']:
                 if i['currency']['iso3'] == self.currency:
                     currency_code.append(i['currency']['iso3'])
@@ -69,25 +35,53 @@ class Currency():
                 currency_buy.append(i['buy'])
                 currency_sell.append(i['sell'])
                 dic_for_table = zip(currency_code, currency_name, currency_buy, currency_sell)
-            
-    
-                # continue
-                
-            
-            # dic_for_table = zip(currency_code, currency_name, currency_buy, currency_sell)
         
         data_frame = pd.DataFrame(dic_for_table, columns=['Currency Code','Currency Name', 'Currency Buy', 'Currency Sell'])
         data_frame.set_index('Currency Code', inplace=True)
         return data_frame.to_markdown()
 
 
-# start_date = input('Enter Start Date: ')
-# last_date = input('Enter Last Date: ')
-# currency = input('Enter Currency Code: ')
+    def convert_to_npr(self, amount):
+        self.amount = amount
+        for i in self.data['data']:
+            
+            for j in self.data['data'][i]['rates']:
+                
+                # Filter the currency from argument and convert it to NPR
+                if j['currency']['iso3'] == self.currency:
+                    print(f"'Currency Name:' {j['currency']['name']}")
+                    print(f"'Currency Buy:' {float(j['buy']) * amount/float(j['currency']['unit'])}")
+                    print(f"'Currency Sell:' {float(j['sell']) * amount/float(j['currency']['unit'])}")
 
-# print(Currency(date=date, currency=currency).table())
+    
 
-print(Currency(date='2023-03-29', currency='').table())
+req = requests.get(f'https://www.nrb.org.np/api/forex/v1/rate/')
+req = req.json()
+count = 1
+curr_dict = {0: 'ALL'}
+
+for i in req['data']['payload']['rates']:
+    curr_dict[count] = i['currency']['iso3']
+    count = count + 1
+
+#====================================================================================================
+                        ######         User Input         ######
+#====================================================================================================
+
+start_date = input('Enter Start Date (Eg: 2023-01-01): ')
+
+print(f"Choose the Currency Code:")
+
+for g in range(len(curr_dict)):
+    print(f"{g} - {curr_dict[g]}")
+
+currency_code_input = int(input(f'Choose the Currency Code:'))
+
+#====================================================================================================
+######      End of User Input         ########
+#====================================================================================================
+
+print(Currency(date='2021-03-28', currency=curr_dict[currency_code_input]).get_data())
 
 # Currency().convert_to_npr(1000)
 
