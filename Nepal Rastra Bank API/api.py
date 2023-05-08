354c94ea5f51f75b7abc7d879664736ca301628f
Rupesh Bhandari <rpshbhandari@gmail.com>
2023-05-08 07:12:44 +0545

    updated

--- Nepal Rastra Bank API/api.py
+++ Nepal Rastra Bank API/api.py
@@ -13,6 +13,30 @@ class Currency:
         self.data = []
         self.total_pages = None
         self.total_records = None
+        
+    def _get_metadata(self):
+        '''This method returns the metadata'''
+        
+        # def wrapper(*args, **kwargs):
+        #     self = args[0]
+        #     response = requests.get(
+        #     f"https://www.nrb.org.np/api/forex/v1/rates/?page=1&from={self.date_from}&to={self.date_to}&per_page=100",
+        #     timeout=60)
+        #     response.raise_for_status()
+        #     self.data = response.json()
+        #     self.total_pages = self.data['pagination']['pages']
+        #     self.total_records = self.data['pagination']['total']
+        #     return func(*args, **kwargs)
+        # return wrapper
+        
+        response = requests.get(
+            f"https://www.nrb.org.np/api/forex/v1/rates/?page=1&from={self.date_from}&to={self.date_to}&per_page=100",
+            timeout=60)
+        response.raise_for_status()
+        self.data = response.json()
+        self.total_pages = self.data['pagination']['pages']
+        self.total_records = self.data['pagination']['total']
+        return self.total_pages, self.total_records
 
     def _get_page(self, page_num):
         '''This method returns the data for a single page'''
@@ -20,42 +44,37 @@ class Currency:
             f"https://www.nrb.org.np/api/forex/v1/rates/?page={page_num}&from={self.date_from}&to={self.date_to}&per_page=100",
             timeout=60)
         response.raise_for_status()
-        data = response.json()
-        self.total_pages = data['pagination']['pages']
-        self.total_records = data['pagination']['total']
-        return data['data']['payload']
+        self.raw_data = response.json()
+        return self.raw_data['data']['payload']
 
     def _parse_data(self, payload):
         '''This method parses the payload and returns a list of dictionaries'''
-        data = []
+        self.parsed_data = []
         for item in payload:
             date = dt.datetime.strptime(item['date'], '%Y-%m-%d').date()
             for rate in item['rates']:
-                data.append({
+                self.parsed_data.append({
                     'date': date,
                     'currency_code': rate['currency']['iso3'],
                     'currency_name': rate['currency']['name'],
                     'buy': rate['buy'],
                     'sell': rate['sell']
                 })
-        return data
+        return self.parsed_data
+    
 
     def get_data(self):
         '''This method returns the data from the API'''
-        
-        self._get_page(1)
-
-        for i in range(1, self.total_pages + 1):
-            payload = self._get_page(i)
-            data = self._parse_data(payload)
-            self.data.extend(data)
-        
-        df = pd.DataFrame(self.data)
-        
-        return df.to_markdown()
+        self._get_metadata()
+        self.data = []
+        for i in range(0, self.total_pages):
+            payload = self._get_page(i+1)
+            self.parsed_data = self._parse_data(payload)
+            self.data.extend(self.parsed_data)
+        data_frame = pd.DataFrame(self.data)
+        return data_frame.to_markdown()
 
 
 if __name__ == "__main__":
-    Curr = Currency(date_from='2023-05-01', date_to='2023-05-04').get_data()
+    Curr = Currency(date_from='2023-05-04', date_to='2023-05-04').get_data()
     print(Curr)
-
